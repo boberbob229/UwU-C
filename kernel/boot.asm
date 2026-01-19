765246e7cdc231b079efa1d69c48509a60581932
bober <boberbob811@gmail.com>
2026-01-18 19:37:05 -0800

    Update boot.asm

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -4,48 +4,50 @@
 KERNEL_OFFSET equ 0x1000
 
 start:
-    ; Setup segments
+    ; RM init
+    cli
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7c00
+    sti
 
-    ; Clear screen (BIOS)
-    mov ax, 0x03
+    ; text mode
+    mov ax, 0x0003
     int 0x10
 
-    ; Load kernel from disk
+    ; BIOS drive
     mov [BOOT_DRIVE], dl
-    
-    mov bx, KERNEL_OFFSET ; Destination
-    mov dh, 32            ; Number of sectors (16KB)
+
+    ; load kernel
+    mov bx, KERNEL_OFFSET
+    mov dh, 32
     mov dl, [BOOT_DRIVE]
     call disk_load
 
-    ; Switch to PM
+    ; rm -> pm
     call switch_to_pm
     jmp $
 
 %include "gdt.asm"
 
-[bits 16]
 disk_load:
     pusha
-    push dx
+    xor ax, ax
+    mov es, ax            ; ES:BX = dst
 
-    mov ah, 0x02 ; BIOS read sector
-    mov al, dh   ; Read DH sectors
-    mov ch, 0x00 ; Cylinder 0
-    mov dh, 0x00 ; Head 0
-    mov cl, 0x02 ; Start from sector 2
+    mov ah, 0x02          ; read
+    mov al, dh            ; sectors
+    mov ch, 0x00          ; cyl
+    mov dh, 0x00          ; head
+    mov cl, 0x02          ; sector
 
     int 0x13
     jc disk_error
-
-    pop dx
     cmp al, dh
     jne disk_error
+
     popa
     ret
 
@@ -55,12 +57,12 @@ disk_error:
     int 0x10
     jmp $
 
-[bits 16]
+
 switch_to_pm:
     cli
     lgdt [gdt_descriptor]
     mov eax, cr0
-    or eax, 0x1
+    or eax, 1
     mov cr0, eax
     jmp CODE_SEG:init_pm
 
@@ -68,18 +70,16 @@ switch_to_pm:
 init_pm:
     mov ax, DATA_SEG
     mov ds, ax
-    mov ss, ax
     mov es, ax
+    mov ss, ax
     mov fs, ax
     mov gs, ax
 
-    mov esp, 0x90000 ; Update stack top
-
-    call KERNEL_OFFSET ; Jump to kernel
+    mov esp, 0x90000      ; stack
+    call KERNEL_OFFSET   ; kernel owns us now
     jmp $
 
 BOOT_DRIVE db 0
 
-; Padding and magic number
 times 510-($-$$) db 0
 dw 0xaa55
